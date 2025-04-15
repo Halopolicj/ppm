aa2b4b4d01957af174c4b47d4605d553abf27949
Halopolicj <xxcien.smiercixx@gmail.com>
2025-04-15 21:12:02 +0200

    Add files via upload

---
+++ src/example/Content/PermaSectorPresets.java
@@ -0,0 +1,20 @@
+package Frozen.Content;
+
+import mindustry.type.SectorPreset;
+public class PermaSectorPresets {
+    public static SectorPreset Beginning;
+
+    public static void load(){
+
+        Beginning = new SectorPreset("Beginning", PermaPlanets.Ice, 15){{
+            captureWave = 10;
+            difficulty = 1;
+
+            alwaysUnlocked = true;
+
+            rules = r -> {
+                r.winWave = captureWave;
+            };
+        }};
+    }
+}
