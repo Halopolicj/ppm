264447a75e23861ffb60ad7bf0a9c6852695dcd7
Halopolicj <xxcien.smiercixx@gmail.com>
2025-04-18 23:05:12 +0200

    Update mod.hjson

--- src/example/Content/PermaBlocks.java
+++ src/example/Content/PermaBlocks.java
@@ -2,6 +2,7 @@ package example.Content;
 
 import mindustry.world.Block;
 import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.OreBlock;
 import mindustry.world.blocks.environment.StaticWall;
 import mindustry.world.meta.Attribute;
 import example.Content.PermaAttributes;
@@ -16,16 +17,23 @@ public class PermaBlocks {
     // walls
     wallGallium;
     public static  void load(){
-        floorNickel = new Floor("floor-nickel-ore", 3) {{
+        floorNickel = new OreBlock("floor-nickel-ore") {{
             itemDrop = nickel;
+            oreDefault = true;
+            variants = 3;
+            oreScale = 10f;
+            oreThreshold = 0.5f;
             attributes.set(Attribute.get("Nickel"), .075f);
             }};
 
         //walls
-        wallGallium = new StaticWall("wall-gallium-ore") {{
-            variants = 3;
+        wallGallium = new OreBlock("wall-gallium-ore") {{
+            variants = 1;
            itemDrop = PermaItems.gallium;
-           attributes.set(Attribute.get("Gallium"), .09f);
+           oreThreshold = 0.4f;
+           oreScale = 1.5f;
+           wallOre = true;
+           //attributes.set(Attribute.get("Gallium"), .09f);
         }};
 
     }
