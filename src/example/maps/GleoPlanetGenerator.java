3b556a0dffce616216329c9b4bc2baabbdbb3236
Halopolicj <xxcien.smiercixx@gmail.com>
2025-05-07 19:38:22 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/example/Content/PermaBlocks.java

---
+++ src/example/maps/GleoPlanetGenerator.java
@@ -0,0 +1,15 @@
+package example.maps;
+
+import mindustry.content.*;
+import mindustry.maps.planet.*;
+import mindustry.world.*;
+
+public class GleoPlanetGenerator extends ErekirPlanetGenerator {
+
+    Block[] terrain = {Blocks.snow, Blocks.ice, Blocks.snowWall, Blocks.redIceWall, Blocks.iceWall};
+
+    {
+        baseSeed = 2;
+
+    }
+}
